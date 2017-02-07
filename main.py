c4fe8d00c463ef8414322b2c9b86c2b4c18d886f
Chris Jaros <cjaros1@att.net>
2017-02-06 19:44:04 -0600

    created main page, created form, created empty validation functions

--- main.py
+++ main.py
@@ -16,10 +16,70 @@
 #
 import webapp2
 
+import cgi
+
+def validate_email(email):
+    isValidated=False
+
+    return isValidated
+
+def validate_password(password):
+    isValidated=False
+
+    return isValidated
+
+def validate_password_confirm(password_confirm,password):
+    isValidated=False
+
+    return isValidated
+
+def vailidate_username(username):
+    isValidated=False
+
+    return isValidated
+
+def validate_form(username, password,password_confirm,email):
+    isValidated=False
+
+    return isValidated
+
+def build_page():
+    header="<h2>User Signup</h2>"
+    username_input = "<label>Username:</label><input name='username' type='text' ></input>"
+    password_input="<label>Password:</label><input type='password' name='password'></input>"
+    password_confirm="<label>Confirm Password:</label><input type='password' name='password_confirm'></input>"
+    email_input="<label>Email Address (optional)</label><input type='text' name=email></input>"
+    submit="<input type='submit'>"
+    form="<form method=post>"+username_input +"<br>"+ password_input + "<br>"+password_confirm+"<br>"+email_input+"<br>"+submit+"</form>"
+    content=header+form
+    return content
+
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content=build_page()
+        self.response.write(content)
+
+    def post(self):
+        username=self.request.get("username")
+        password=self.request.get("password")
+        password_confirm=self.request.get("password_confirm")
+        email=self.request.get("email")
+        user_error=""
+        password_error=""
+        password_confirm_error=""
+        email_error=""
+
+        vailidate_username(username)
+        validate_password(password)
+        validate_password_confirm(password_confirm,password)
+        validate_form(username, password, password_confirm, email)
+
+
+        content=build_page()
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler)
 ], debug=True)
